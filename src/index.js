015310de421f2bfe60ceaf88398a66c3a027274a
shunkominato <shun19871001@gmail.com>
2022-06-02 22:13:31 +0900

    a

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+import './style.css'
+
+function component() {
+  const element = document.createElement('div');
+  const array = ['Hello', 'webpack', '!!']
+  element.innerHTML = _.join(array, ' ')
+  return element;
+}
+
+document.body.appendChild(component());
+document.body.classList.add('haikei');
