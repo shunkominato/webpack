b04b8aa9612eb91ea5c529ca532acf970aaa5188
shunkominato <shun19871001@gmail.com>
2022-06-04 09:27:52 +0900

    f

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import _ from 'lodash'
-import './style.css'
-import logo from './logo.png'
+import React from 'react';
+import ReactDom from 'react-dom';
 
-function component() {
-  const element = document.createElement('div');
-  const array = ['Hello', 'webpack', '!!']
-  element.innerHTML = _.join(array, ' ')
-  return element;
-}
-
-document.body.appendChild(component());
-document.body.classList.add('haikei');
-
-const image = new Image()
-image.src = logo
-document.body.appendChild(image)
+ReactDom.render(
+  <div>reactr</div>,
+  document.getElementById('root')
+)
